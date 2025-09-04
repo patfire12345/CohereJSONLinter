6e6e3ce6d616e4dda4fca6765518197880e7b12a
Patrick Huang <phuan097@uottawa.ca>
2025-09-04 17:06:11 -0400

    <update> Removed hasDuplicateKey function and implemented non-whitespace string checking for duplicate keys

--- js/script_JSON.js
+++ js/script_JSON.js
@@ -40,65 +40,24 @@ document.addEventListener("mouseup", () => {
   document.body.style.cursor = "default";
 });
 
-function hasDuplicateKey(jsonString) {
-  let splitData = jsonString.split(",");
-  for (let i = 0; i < splitData.length; i++) {
-    let splitDataList = splitData[i].split('"');
-    splitData[i] = splitDataList[1];
-  }
-
-  let uniqueKeySet = new Set();
-  for (let j = 0; j < splitData.length; j++) {
-    if (uniqueKeySet.has(splitData[j])) {
-      return true;
-    }
-
-    if (splitData[j] !== undefined) {
-      if (splitData[j].length !== 0) {
-        uniqueKeySet.add(splitData[j]);
-      }
-    }
-  }
-
-  return false;
-}
-
 function lintJSON() {
   try {
     var rawData = editor.getValue(); // Get JSON data from editor
     var data = JSON.parse(rawData); // Parse JSON to validate
     var formattedJSON = JSON.stringify(data, null, 4); // Reformat JSON with 4-space indentation
 
-    // let hasDuplicate = false;
-    // let start = [];
-    // let i = 0;
-    // while (i < rawData.length) {
-    //   if (rawData[i] === "{") {
-    //     start.push(i);
-    //   } else if (rawData[i] === "}") {
-    //     let start_i = start.pop();
-    //     let stringLen = rawData.slice(start_i, i).length;
+    let hasDuplicate =
+      rawData.replace(/\s/g, "").length !==
+      formattedJSON.replace(/\s/g, "").length;
 
-    //     if (hasDuplicateKey(rawData.slice(start_i, i))) {
-    //       hasDuplicate = true;
-    //       break;
-    //     } else {
-    //       rawData = rawData.slice(0, start_i) + rawData.slice(i + 1);
-    //       i -= stringLen;
-    //     }
-    //   }
-
-    //   i++;
-    // }
-
-    // if (hasDuplicate === true) {
-    //   document.getElementById("output").innerHTML =
-    //     '<div class="alert alert-warning" role="alert">Duplicate keys found.</div>';
-    // } else {
-    editor.setValue(formattedJSON, -1); // Set formatted JSON back to editor, -1 moves cursor to the start
-    document.getElementById("output").innerHTML =
-      '<div class="alert alert-success" role="alert">Valid JSON! Formatted successfully.</div>';
-    // }
+    if (hasDuplicate === true) {
+      document.getElementById("output").innerHTML =
+        '<div class="alert alert-warning" role="alert">Duplicate keys found.</div>';
+    } else {
+      editor.setValue(formattedJSON, -1); // Set formatted JSON back to editor, -1 moves cursor to the start
+      document.getElementById("output").innerHTML =
+        '<div class="alert alert-success" role="alert">Valid JSON! Formatted successfully.</div>';
+    }
   } catch (error) {
     document.getElementById("output").innerHTML =
       '<div class="alert alert-danger" role="alert">Invalid JSON: ' +
